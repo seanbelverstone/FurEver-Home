8e3911fa1ac3b0d8fc516b6e7e3c7c321df41f16
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-14 11:17:04 -0500

    Created the layout for the jumbotron component and also a components folder

---
+++ client/src/components/jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+
+function Jumbotron() {
+    return(
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">FurEver Home</h1>
+                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
