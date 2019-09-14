df40f179938d98ff52de4931e06f2a34985ba0ce
Scott Finlay <xisifer@gmail.com>
2019-09-14 14:13:42 -0500

    Got an initial rough draft of the pet DataCard up and rendering successfully. CSS still needs some work. Pet data is local dummy test data from a .json, instead of an actual live API call.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import animal from "../src/animals.json";
+import CharCard from "./components/datacard.js";
 
 class App extends Component {
+  // Setting this.state.chars to the chars json array
+  state = {
+    // Initialize and import the chars array
+    animal: animal,
+    // Create an array storing the ID of Favorites
+    favArray: []
+  };
+
+
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
+        <div id="cardarea">
+        {this.state.animal.map(animal => (
+          <CharCard
+           { ...animal }
+            // id={animal.id}
+            // key={animal.id}
+            // name={animal.name}
+            // image={animal.image}
+            // type={animal.type}
+            // gender={animal.gender}
+            // description={animal.description}
+            // spayed_neutered={animal.spayed_neutered}
+            // shots_current={animal.shots_current}
+            // children={animal.children}
+            // email={animal.email}
+            // phone={animal.phone}
+            // occupation={char.occupation}
+            // location={char.location}
+          />
+        ))}
+        </div>
+
+
+      {/* <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,7 +48,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </div> */}
+      </React.Fragment>
     );
   }
 }
