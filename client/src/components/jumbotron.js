1cd67c3a0edc43c27672f5c59bae0816272f9ad5
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-14 13:45:13 -0500

    Switched everything over to reactstrap. Finished the jumbotron (mainly);

--- client/src/components/jumbotron.js
+++ client/src/components/jumbotron.js
@@ -1,20 +1,21 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import {Jumbotron, Button, Container} from "reactstrap";
 import "../App.css";
 
 const mainJumbotron = (props) => {
-    return(
-        // Jumbotron
-        <div>
-        <Jumbotron fluid>
-          <Container fluid>
-            <h1 className="display-3">Fluid jumbotron</h1>
-            <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-            <Button id="login" color="primary">Learn More</Button>
-          </Container>
-        </Jumbotron>
-      </div>
-    );
-}
+	return(
+	// Jumbotron
+		<div>
+			<Jumbotron fluid>
+				<Container fluid>
+					<h1 className="display-3">FurEver Home</h1>
+					<p className="lead">Find your next best friend</p>
+					<Button id="login" color="primary">Login</Button>
+				</Container>
+			</Jumbotron>
+		</div>
+	);
+};
 
 export default mainJumbotron;
