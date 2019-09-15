90fb30698a990f9bc6442f612e7651e2ab0ef5a6
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-15 18:36:56 -0500

    Removed ndoe modules

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,61 +1,61 @@
-import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup } from 'reactstrap';
+import React from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup } from "reactstrap";
 import "../App.css";
 
 class ModalExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal: false,
-      backdrop: true
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			modal: false,
+			backdrop: true
+		};
 
-    this.toggle = this.toggle.bind(this);
-    this.changeBackdrop = this.changeBackdrop.bind(this);
-  }
+		this.toggle = this.toggle.bind(this);
+		this.changeBackdrop = this.changeBackdrop.bind(this);
+	}
 
-  toggle() {
-    this.setState(prevState => ({
-      modal: !prevState.modal
-    }));
-  }
+	toggle() {
+		this.setState(prevState => ({
+			modal: !prevState.modal
+		}));
+	}
 
-  changeBackdrop(e) {
-    let value = e.target.value;
-    if (value !== 'static') {
-      value = JSON.parse(value);
-    }
-    this.setState({ backdrop: value });
-  }
+	changeBackdrop(e) {
+		let value = e.target.value;
+		if (value !== "static") {
+			value = JSON.parse(value);
+		}
+		this.setState({ backdrop: value });
+	}
 
-  render() {
-    return (
-      <div>
-        <Form inline onSubmit={(e) => e.preventDefault()}>
-          <FormGroup>
-            <Label for="backdrop">Backdrop value</Label>{' '}
-            <Input type="select" name="backdrop" id="backdrop" onChange={this.changeBackdrop}>
-              <option value="true">true</option>
-              <option value="false">false</option>
-              <option value="static">"static"</option>
-            </Input>
-          </FormGroup>
-          {' '}
-          <Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
-        </Form>
-        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} backdrop={this.state.backdrop}>
-          <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-          <ModalBody>
+	render() {
+		return (
+			<div>
+				<Form inline onSubmit={(e) => e.preventDefault()}>
+					<FormGroup>
+						<Label for="backdrop">Backdrop value</Label>{" "}
+						<Input type="select" name="backdrop" id="backdrop" onChange={this.changeBackdrop}>
+							<option value="true">true</option>
+							<option value="false">false</option>
+							<option value="static">"static"</option>
+						</Input>
+					</FormGroup>
+					{" "}
+					<Button color="danger" onClick={this.toggle}>{this.props.buttonLabel}</Button>
+				</Form>
+				<Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className} backdrop={this.state.backdrop}>
+					<ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+					<ModalBody>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
-      </div>
-    );
-  }
+					</ModalBody>
+					<ModalFooter>
+						<Button color="primary" onClick={this.toggle}>Do Something</Button>{" "}
+						<Button color="secondary" onClick={this.toggle}>Cancel</Button>
+					</ModalFooter>
+				</Modal>
+			</div>
+		);
+	}
 }
 
 export default ModalExample;
