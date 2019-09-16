1159d03767fb6d483023f1749181008079259b6f
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-16 17:00:24 -0500

    Renamed the ids on the input on login.js to name and password, to match the database being created by Dan

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Button, Form, FormGroup, Input } from 'reactstrap';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { Button, Form, FormGroup, Input } from "reactstrap";
 
 export default class Example extends React.Component {
-  render() {
-    return (
-      <Form>
-        <FormGroup>
-          <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
-        </FormGroup>
-        <FormGroup>
-          <Input type="password" name="password" id="examplePassword" placeholder="Password" />
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
-    );
-  }
+	render() {
+		return (
+			<Form>
+				<FormGroup>
+					<Input type="email" name="email" id="name" placeholder="Email" />
+				</FormGroup>
+				<FormGroup>
+					<Input type="password" name="password" id="password" placeholder="Password" />
+				</FormGroup>
+				<Button>Submit</Button>
+			</Form>
+		);
+	}
 }
