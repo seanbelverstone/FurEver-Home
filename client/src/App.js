1457712488c1925de4f573ae246b928d4787181e
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-15 19:56:01 -0500

    Added some id names for easy selection. Added styling and created an images folder for easy access to files

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+
         <Jumbotron />
-      
         <div id="cardarea">
         {this.state.animal.map(animal => (
           <CharCard
--- client/src/components/jumbotron.js
+++ client/src/components/jumbotron.js
@@ -8,12 +8,12 @@ const mainJumbotron = (props) => {
 	return(
 	// Jumbotron
 		<div>
-			<Jumbotron fluid>
+			<Jumbotron fluid id="jumbotron">
 				<Container fluid>
+					<Modal>Login</Modal>
 					<h1 className="display-3">FurEver Home</h1>
 					<p className="lead">Find your next best friend</p>
 					{/* This button needs to float to the right */}
-					<Modal>Login</Modal>
 				</Container>
 			</Jumbotron>
 		</div>
