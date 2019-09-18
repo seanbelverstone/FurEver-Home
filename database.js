d7d53d2b87626ffbb27ccda7169961a3018a11e3
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-17 22:07:48 -0500

    Made some changes in master by accident. Whoops

--- database.js
+++ database.js
@@ -1,7 +1,12 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("mysql://root:root@localhost:3307/furever-home", {
-	define: {
-		timestamps: false
+const sequelize = new Sequelize("username", "email", "password", {
+	host: "localhost",
+	dialect: "mysql",
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
 	}
 });
 
--- server.js
+++ server.js
@@ -3,13 +3,26 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const User = require("./sequelize/user");
+const db = require("./sequelize/database");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
+
+app.post("/api/users", (request, response) => {
+	db.User.create({
+		name: request.body.name,
+		email: request.body.email,
+		password: request.body.password
+	}).then(newUser => {
+		response.json(newUser);
+	});
+
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
@@ -21,14 +34,4 @@ app.listen(PORT, function() {
 	console.log(`🌎 ==> API server now on port ${PORT}!!`);
 });
 
-app.post("/api/users", (request, response) => {
-	User.create({
-		name: request.body.name,
-		email: request.body.email,
-		password: request.body.password
-	}).then(newUser => {
-		response.json(newUser);
-	});
-
-});
 
