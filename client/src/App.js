c9d38b7a99c19714b7ec736ea35d33fd4b56cda3
Danniel Zelaya <dannielzelaya@gmail.com>
2019-09-18 18:54:34 -0500

    do no use this commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,48 @@
+/* eslint-disable no-unused-vars */
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Jumbotron from "./components/jumbotron";
 import "./App.css";
+import animals from "../src/animals.json";
+import CharCard from "./components/datacard.js";
+import axios from "axios";
+// import CardBodyInfo from "./components/CardBodyInfo.js";
+import FilterBar from "./components/sidebar.js";
+import FavBar from "./public/src/components/FavBar.js";
 
 class App extends Component {
+  // Setting this.state.chars to the chars json array
+  state = {
+    // Initialize and import the chars array
+    animals: animals,
+    // Create an array storing the ID of Favorites
+    favArray: [],
+    users: []
+  };
+  componentDidMount() {
+    axios
+      .get("api/pets")
+      .then(response => response.data)
+      .then(data => {
+        this.setState({ users: data });
+        // console.log(this.state.users)
+        // console.log(data)
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <React.Fragment>
+        <Jumbotron />
+        <FilterBar />
+        <FavBar />
+        <div className="charCard">
+          {this.state.animals.map(animal => (
+            <div>
+              <CharCard animal={animal} />
+            </div>
+          ))}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </React.Fragment>
     );
   }
 }
