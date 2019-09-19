562174ed23553f1052ec8476963c42b838a83301
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-18 20:20:24 -0500

    Deleted the duplicate user and database files

--- sequelize/database.js
+++ sequelize/database.js
@@ -1,8 +1,22 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("mysql://root:root@localhost:3307/furever-home", {
-	define: {
-		timestamps: false
-	}
-});
+const env = process.env.NODE_ENV || "development";
+const config = require("./client/config/config.json")[env];
+
+if (config.use_env_variable) {
+	var sequelize = new Sequelize (process.env[config.use_env_variable]);
+} else {
+	var sequelize = new Sequelize(config.database, config.username, config.password, { //config.email,
+		host: "localhost",
+		dialect: "mysql",
+		pool: {
+			max: 5,
+			min: 0,
+			acquire: 30000,
+			idle: 10000
+		}
+	});
+};
+ 
+
 
 module.exports = sequelize;
